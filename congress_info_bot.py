fa3877d7d448d90ca5771b12ac2818e114e15318
gtwillwin <gtwillwin@gmail.com>
2017-03-04 01:05:22 -0500

    Add functionality to parse .yaml for legislator contact information and fix issue where bot would continue to reply to same comment over and over again

--- congress_info_bot.py
+++ congress_info_bot.py
@@ -17,24 +17,52 @@ def authenticate():
     return reddit
 
 
-def run_bot():
+def run_bot(replied_to):
     reddit = authenticate()
-    names = get_legislators()
-    for mention in reddit.inbox.mentions(limit=25):
-        for name in names:
-            if name in mention.body:
-                mention.reply(
-                    "Placeholder"
-                    )
+    legislators = get_legislator_name()
+    while True:
+        for mention in reddit.inbox.mentions(limit=25):
+            if mention.id not in replied_to:
+                for legislator in legislators:
+                        if legislator['name']['official_full'] in mention.body:
+                            mention.reply(
+                                get_legislator_info(legislator)
+                            )
+                            replied_to.append(mention.id)
+                            with open("replied_to.txt", "a") as f:
+                                f.write(mention.id + "\n")
         time.sleep(10)
 
 
-def get_legislators():
+def get_legislator_name():
     response = requests.get('https://raw.githubusercontent.com/unitedstates/congress-legislators/master/legislators-current.yaml')
     legislators = yaml.load(response.content)
-    names = legislators[0]['name']['official_full']
-    return names
+    return legislators
+
+
+def get_legislator_info(legislator):
+        return (
+            "**{name}**\n\n Phone: {phone}\n\n Fax: {fax} \n\n Address: {address}".format(
+                name=legislator['name']['official_full'],
+                phone=legislator['terms'][-1]['phone'],
+                address=legislator['terms'][-1]['address'],
+                fax=legislator['terms'][-1]['fax']
+            )
+        )
+
+
+def get_saved_mentions():
+    if not os.path.isfile("replied_to.txt"):
+        replied_to = []
+    else:
+        with open("replied_to.txt", "r") as f:
+            replied_to = f.read()
+            replied_to = replied_to.split("\n")
+    return replied_to
+
+
+replied_to = get_saved_mentions()
 
 
 if __name__ == '__main__':
-    run_bot()
+    run_bot(replied_to)
