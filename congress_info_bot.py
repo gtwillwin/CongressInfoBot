32534cddc8b267dd2fdf3a921004653229444668
gtwillwin <gtwillwin@users.noreply.github.com>
2017-03-08 20:36:27 -0500

    Update congress_info_bot.py

--- congress_info_bot.py
+++ congress_info_bot.py
@@ -1,6 +1,6 @@
 # CongressInfoBot v1.0
 # Created by gtwillwin
-
+ 
 import praw
 import time
 import os
@@ -8,10 +8,10 @@ import requests
 import yaml
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
-
+ 
 import config
 from util import filter
-
+ 
 def authenticate():
     reddit = praw.Reddit(
         username=config.username,
@@ -21,46 +21,42 @@ def authenticate():
         user_agent="Congress Info Bot v1.0"
     )
     return reddit
-
-
+ 
+ 
 def run_bot(replied_to):
     reddit = authenticate()
     legislators = get_legislators()
     names = [leg['name']['official_full'] for leg in legislators]
     start = '"'
     end = '"'
-
+ 
     while True:
         for mention in reddit.inbox.mentions(limit=25):
             if mention.id not in replied_to:
                 mark_applied(mention.id)
                 name = mention.body.split(start)[1].split(end)[0]
                 person = process.extractOne(name, names, scorer=fuzz.partial_ratio)[0]
-                matching_leg = filter(legislator_match, legislators)
-
+                matching_leg = filter(lambda x: person == x['name']['official_full'], legislators)
+ 
                 if matching_leg:
                     mention.reply(get_legislator_info(matching_leg))
                            
         time.sleep(10)
-
-
+ 
+ 
 def mark_applied(comment_id):
     with open("replied_to.txt", "a") as f:
         f.write(comment_id + "\n")
     replied_to.append(comment_id)
-
-
+ 
+ 
 def get_legislators():
     response = requests.get(
         'https://raw.githubusercontent.com/unitedstates/congress-legislators/master/legislators-current.yaml'
     )
     return yaml.load(response.content)
-
-
-def legislator_match(name, legislator):
-    return name == legislator['name']['official_full']
-
-
+ 
+ 
 def get_legislator_info(legislator):
         return (
             "##{title} {name} ({party}-{state}) \n\n"
@@ -83,11 +79,11 @@ def get_legislator_info(legislator):
                 state=legislator['terms'][-1].get('state', 'N/A'),
                 govtrack=legislator['id'].get('govtrack', 'N/A'),
                 votesmart=legislator['id'].get('votesmart', 'N/A')
-
+ 
             )
         )
-
-
+ 
+ 
 def get_saved_mentions():
     if not os.path.isfile("replied_to.txt"):
         replied_to = []
@@ -95,12 +91,12 @@ def get_saved_mentions():
         with open("replied_to.txt", "r") as f:
             replied_to = f.read()
             replied_to = replied_to.split("\n")
-
+ 
     return replied_to
-
-
+ 
+ 
 replied_to = get_saved_mentions()
-
-
+ 
+ 
 if __name__ == '__main__':
     run_bot(replied_to)
